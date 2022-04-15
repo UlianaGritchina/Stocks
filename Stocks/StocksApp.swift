70e04833ac94916184a399b1d0c7b503c9211f69
UlianaGritchina <ugritchina@icloud.com>
2022-04-15 10:02:42 +0300

    Initial Commit

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Ульяна Гритчина on 15.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
